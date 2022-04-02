185ec62afc1b8b4db9b180e9978220e6016bb616
ligalaz <89035405+ligalaz@users.noreply.github.com>
2022-04-02 23:30:09 +0300

    feat:add dns solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,32 +1,29 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
 
-/**
- * Given an array of domains, return the object with the appearances of the DNS.
- *
- * @param {Array} domains
- * @return {Object}
- *
- * @example
- * domains = [
- *  'code.yandex.ru',
- *  'music.yandex.ru',
- *  'yandex.ru'
- * ]
- *
- * The result should be the following:
- * {
- *   '.ru': 3,
- *   '.ru.yandex': 3,
- *   '.ru.yandex.code': 1,
- *   '.ru.yandex.music': 1,
- * }
- *
- */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let container = []
+  let result = {}
+
+  domains = domains.map((item) =>
+    item
+      .split(".")
+      .reverse()
+      .map((item) => "." + item)
+  )
+
+  for (let i = 0; i < domains.length; i++) {
+    let str = ""
+    for (let j = 0; j < domains[i].length; j++) {
+      str += domains[i][j]
+      container.push(str)
+    }
+  }
+
+  container.forEach((item) => (result[item] = (result[item] || 0) + 1))
+
+  return result
 }
 
 module.exports = {
-  getDNSStats
-};
+  getDNSStats,
+}
