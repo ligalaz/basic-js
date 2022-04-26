a65b53f94a9485aed25c0ee9d89bdcc4b11c0634
ligalaz <89035405+ligalaz@users.noreply.github.com>
2022-04-27 01:21:04 +0300

    fix:correct solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,37 +1,24 @@
 const { NotImplementedError } = require("../extensions/index.js")
 
-/**
- * Create a repeating string based on the given parameters
- *
- * @param {String} str string to repeat
- * @param {Object} options options object
- * @return {String} repeating string
- *
- *
- * @example
- *
- * repeater('STRING', { repeatTimes: 3, separator: '**',
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
 function repeater(str, options) {
-  let addition, additionSeparator, additionRepeatTimes, separator, repeatTimes
+  let addition, firstHalf
 
-  str = String(str)
+  options["addition"] === false || options["addition"] === null
+    ? (addition = String(options["addition"]) || "")
+    : (addition = options["addition"] || "")
 
-  addition = options["addition"] || ""
-  additionSeparator = options["additionSeparator"] || "|"
-  additionRepeatTimes = options["additionRepeatTimes"] || 1
-  separator = options["separator"] || "+"
-  repeatTimes = options["repeatTimes"] || 1
+  let additionSeparator = options["additionSeparator"] || "|"
+  let additionRepeatTimes = options["additionRepeatTimes"] || 1
+  let separator = options["separator"] || "+"
+  let repeatTimes = options["repeatTimes"] || 1
 
-  addition = (addition + `${additionSeparator}`)
-    .repeat(additionRepeatTimes)
-    .slice(0, addition.length - additionSeparator.length)
-  return (str + addition + `${separator}`)
-    .repeat(repeatTimes)
-    .slice(0, separator.length)
+  function halfRepeater(how, what, than) {
+    return new Array(how).fill(what).join(than)
+  }
+
+  firstHalf = halfRepeater(additionRepeatTimes, addition, additionSeparator)
+
+  return halfRepeater(repeatTimes, `${str}${firstHalf}`, separator)
 }
 
 module.exports = {
